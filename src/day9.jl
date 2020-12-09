2efc93ffacdf9d7afa8389bcdfd5e92168943d73
keutoi <k3tu0isui@gmail.com>
2020-12-09 11:28:31 +0530

    day9: simple search problems

---
+++ src/day9.jl
@@ -0,0 +1,42 @@
+function getnums(file)
+    map(l -> parse(Int, l), readlines(file))
+end
+
+function isvalidnum(num, preamble)
+    sp = sort(preamble)
+    findfirst(e -> length(searchsorted(sp, num - e)) !== 0, sp)
+end
+
+function checknums(nums, size)
+    preamble = nums[1:size]
+    rest = nums[size+1:length(nums)]
+    for n in rest
+#        print(preamble, "\n")
+#        print(n, "\n")
+        if isvalidnum(n, preamble) == nothing
+            return n
+        else
+            
+            preamble[1] = n
+            preamble = circshift(preamble, -1)
+        end
+    end
+end
+
+function find_cont_sum(sum, nums)
+    len = length(nums)
+    i = 1
+    while (i <= len)
+        j = i
+        jsum = 0
+        while (j <= len && jsum <= sum)
+            if jsum == sum
+                return maximum(nums[i:j-1]) + minimum(nums[i:j-1])
+            else
+                jsum += nums[j]
+            end
+            j += 1
+        end
+        i += 1
+    end
+end
